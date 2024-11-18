dff8868ad4a51f69dec955dee2d5e3848c66076b
Maxence <coddyumpro@gmail.com>
2024-11-18 12:14:49 +0100

    rm database en prevision de passer a supabase

--- src/createNewRole/CreateNewRole.jsx
+++ src/createNewRole/CreateNewRole.jsx
@@ -3,68 +3,21 @@ import RoleList from "../roleList/RoleList";
 import api from "../api";
 
 export default function CreateNewRole() {
-    const [name, setName] = useState("");
-    const [color, setColor] = useState("#000000");
-    const [roles, setRoles] = useState([]);
-
-    // Récupérer les rôles au chargement du composant
-    useEffect(() => {
-        const fetchRoles = async () => {
-            try {
-                const response = await api.get("/roles");
-                setRoles(response.data);
-            } catch (error) {
-                console.error("Error fetching roles:", error.response.data);
-            }
-        };
-
-        fetchRoles();
-    }, []);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await api.post("/roles", { name, color });
-            console.log("Role Created:", response.data);
-
-            // Réinitialisation des inputs
-            setName("");
-            setColor("#000000");
-
-            // Ajout du rôle créé à la liste existante
-            setRoles((prevRoles) => [...prevRoles, response.data]);
-        } catch (error) {
-            console.error("Error Creating Role:", error.response.data);
-        }
-    };
-
     return (
         <>
             <div className="newRole">
                 <h2>Create New Role</h2>
-                <form onSubmit={handleSubmit}>
+                <form>
                     <div className="formInput">
-                        <input
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                            placeholder="Role Name"
-                        />
-                        <input
-                            type="color"
-                            value={color}
-                            onChange={(e) => setColor(e.target.value)}
-                            required
-                        />
+                        <input type="text" required placeholder="Role Name" />
+                        <input type="color" required />
                     </div>
                     <button type="submit">Add New Role</button>
                 </form>
             </div>
 
             {/* Passer roles et setRoles à RoleList */}
-            <RoleList roles={roles} setRoles={setRoles} />
+            <RoleList />
         </>
     );
 }
--- src/roleList/RoleList.jsx
+++ src/roleList/RoleList.jsx
@@ -1,19 +1,6 @@
 import React from "react";
-import axios from "axios";
-import api from "../api";
 
 const RoleList = ({ roles, setRoles }) => {
-    const handleDelete = async (id) => {
-        try {
-            await axios.delete(`http://localhost:5001/api/roles/${id}`);
-
-            // Mise à jour des rôles après suppression
-            setRoles((prevRoles) => prevRoles.filter((role) => role.id !== id));
-        } catch (error) {
-            console.error("Erreur lors de la suppression du rôle : ", error);
-        }
-    };
-
     return (
         <div className="roleslist">
             <h2>Liste des rôles</h2>
@@ -24,11 +11,7 @@ const RoleList = ({ roles, setRoles }) => {
                         className="role-item"
                         style={{ borderColor: role.color, color: role.color }}>
                         {role.name}
-                        <button
-                            className="rmRole"
-                            onClick={() => handleDelete(role.id)}>
-                            x
-                        </button>
+                        <button className="rmRole">x</button>
                     </li>
                 ))}
             </ul>
